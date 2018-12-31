06af0a1bc3f8eb106c5e2b2e24218c0fb02be80f
aqua25 <h.aqua25@gmail.com>
2018-12-31 17:27:37 +0900

    Editing group_side_bar haml routing controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,24 +5,9 @@ class MessagesController < ApplicationController
     @messages = Message.all
   end
 
-  def new
-  end
-
   def create
     Message.create(text: message_params[:text],image: message_params[:image])
     redirect_to action: :index
-  end
-
-  def show
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,7 @@
 class UsersController < ApplicationController
 
-  def new
-  end
-
-  def create
-  end
-
   def edit
     @users = User.find(params[:id])
   end
 
-  def update
-  end
-
-  def destroy
-  end
 end
